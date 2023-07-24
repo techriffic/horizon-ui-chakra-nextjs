bbbb738a8f26b0f4d915ff7ba99c505058ac55b9
t3chriffic <socials@techriffic.com>
2023-07-24 16:57:24 +0500

    212312sd

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = { 
+    apps: [{ 
+        name: 'techriffic',
+        script: 'npm start',
+        env: {
+       PORT: 3001
+     }
+    }], 
+ 
+    deploy: { 
+        production: { 
+            key: 'id_rsa', 
+            user: 'newadmin', 
+            host: '138.3.240.170', 
+            ref: 'origin/main', 
+            repo: 'git@github.com:techriffic/horizon-ui-chakra-nextjs.git', 
+            path: '/home/admin/web/jugaartech.com/nodeapp', 
+            'pre-deploy-local': '', 
+            'post-deploy': 'cd /home/admin/web/jugaartech.com/nodeapp && source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production', 
+            'pre-setup': '', 
+            'ssh_options': 'ForwardAgent=yes' 
+        } 
+    } 
+}
